0effb07a89e9a70f8f99abf90798118a02eaadbe
Macky0523 <71018332+Macky0523@users.noreply.github.com>
2023-01-27 18:13:47 +0800

    Add files via upload

---
+++ sundoadmin/unpaid.php
@@ -0,0 +1,45 @@
+
+<?php
+header("Access-Control-Allow-Origin: *"); //add this CORS header to enable any domain to send HTTP requests to these endpoints:
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "sundoadmin"; 
+$id = '';
+ 
+$con = mysqli_connect($host, $user, $password,$dbname);
+ 
+$method = $_SERVER['REQUEST_METHOD'];
+ 
+ 
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+ 
+ 
+switch ($method) {
+    case 'GET':
+      $sql = "select * from booking where Status = 0"; 
+      break;
+}
+ 
+// run SQL statement
+$result = mysqli_query($con,$sql);
+ 
+// die if SQL statement failed
+if (!$result) {
+  http_response_code(404);
+  die(mysqli_error($con));
+}
+ 
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+}else {
+    echo mysqli_affected_rows($con);
+}
+ 
+$con->close();
